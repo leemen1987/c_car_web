9fb3794932eeac4257d4be843c6146d204417389
leemen1987 <44331856+leemen1987@users.noreply.github.com>
2026-06-06 10:54:28 +0800

    add App.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <router-view />
+</template>
+
+<script setup>
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  font-family: 'Microsoft YaHei', sans-serif;
+}
+#app {
+  height: 100vh;
+}
+</style>
